78f7ddb5dcd66e251f14eb2cea3d5e682e5d1f6c
ldcas-uc3m <100429021@alumnos.uc3m.es>
2023-02-20 16:57:35 +0100

    wip

--- src/crack.sh
+++ src/crack.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-JOHN_PATH="~/src/john/run/john"
-JOHN_CONF_PATH="~/src/john/run/john.conf"
-JOHN_POT_PATH="~/src/john/run/john.pot"
+JOHN_PATH=$HOME"/src/john/run/john"
+JOHN_CONF_PATH=$HOME"/src/john/run/john.conf"
+JOHN_POT_PATH=$HOME"/src/john/run/john.pot"
 
 
 # extract hashes
@@ -16,21 +16,31 @@ cp $JOHN_CONF_PATH tmp/john.conf
 
 sed -i '4193 i .include "custom_rules.conf"' $JOHN_CONF_PATH
 
-# generate the dictionary
+# generate the dictionaries
 cewl -v -d 2 https://www.lab.inf.uc3m.es -w dictionaries/lab-inf-uc3m
 
-cat dictionaries/* >> tmp_dictionary
+# base dictionary
+cat dictionaries/hotmail.txt >> tmp_dictionary
+cat dictionaries/top-500-worst-passwords >> tmp_dictionary
 sort tmp_dictionary | uniq  > dictionary  # remove duplicates
 rm tmp_dictionary
 
 # rules
-$JOHN_PATH --rules=Custom --wordlist=dictionary --stdout | sort | uniq > rule_dictionary
+
+# pre-rule dictionary
+cat dictionaries/lab-inf-uc3m > tmp_dictionary
+cat dictionaries/spanish-names >> tmp_dictionary
+sort tmp_dictionary | uniq  > pre_rule_dictionary
+rm tmp_dictionary
+
+
+$JOHN_PATH --rules=Custom --wordlist=pre_rule_dictionary --stdout | sort | uniq > rule_dictionary
 
 # run
 $JOHN_PATH --wordlist=rule_dictionary --max-length=15 hashes.hash
 
 # run with GPU (mighht need to install driver for OpenCL)
-# john --format=raw-MD5-opencl --wordlist=rule_dictionary --max-length=15 hashes.hash
+# $JOHN_PATH --format=raw-MD5-opencl --wordlist=rule_dictionary --max-length=15 hashes.hash
 
 # copy the pot 
 $JOHN_PATH --show output
